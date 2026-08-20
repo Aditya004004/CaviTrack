95b1d8a2e8ec72cc8984f70dbcbb8bba4ab23119
Aditiya <adityamaurya0361@gmail.com>
2026-08-20 23:52:48 +0530

    Revert addition of kotlin.android plugin to fix build failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.io.FileInputStream
 plugins {
     alias(libs.plugins.google.services)
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.serialization")
